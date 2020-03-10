d1ab92764098ee0f5f75fce07da0db81fc937bbc
hung.vm2574@gmail.com <riley-guest@Riley-guests-MacBook-Air.local>
2020-03-10 16:42:32 +1100

    adding vehicle

---
+++ src/component/CarImage.tsx
@@ -0,0 +1,33 @@
+import * as React from "react"
+import { useState } from "react";
+import styled from "styled-components"
+interface carImage{
+    upLoadImage(image: any): void;
+}
+const Img = styled.img`
+  width: 190pt;
+  height: 185pt;
+  margin-top: 10%;
+  @media (min-width: 850px) {
+    width: 390pt;
+    height: 285pt;
+    margin-top: 10%;
+  }
+`;
+const Input = styled.input`
+margin-top: px;`
+const CarImage: React.FC<carImage>= props =>{
+    const initialUrl=""
+    const [url, setUrl] = useState<string>(initialUrl)
+    const handleChange= (e: any)=>{
+        setUrl(URL.createObjectURL(e.target.files[0]))
+        props.upLoadImage(e.target.files[0])
+    }
+
+    return <div>
+        <Img src={url}></Img>
+        <Input type="file" onChange={handleChange}></Input>
+        
+    </div>
+}
+export default CarImage;
---
+++ src/firebase/index.tsx
@@ -0,0 +1,21 @@
+import * as firebase from "firebase/app";
+import   "firebase/storage";
+
+import "firebase/storage";
+let firebaseConfig = {
+    apiKey: "AIzaSyD1jhzXOBgPIpgW0HWfvpHk9Lwg9zb2dKc",
+    authDomain: "myvan-12eb8.firebaseapp.com",
+    databaseURL: "https://myvan-12eb8.firebaseio.com",
+    projectId: "myvan-12eb8",
+    storageBucket: "myvan-12eb8.appspot.com",
+    messagingSenderId: "193318999071",
+    appId: "1:193318999071:web:6f70d36b03a18cf5d4f12a",
+    measurementId: "G-6XWD1DQ5M8"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  //firebase.analytics();
+  const storage = firebase.storage();
+  export {
+      storage, firebase as default
+  }
